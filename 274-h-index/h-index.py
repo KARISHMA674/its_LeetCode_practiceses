7e5b121753e19889a026932aaa2f473936f6dfd6
karishma <161018216+KARISHMA674@users.noreply.github.com>
2026-08-16 00:08:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (76.73%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        buckets = [0] * (n + 1)
+        
+        
+        for c in citations:
+            if c >= n:
+                buckets[n] += 1
+            else:
+                buckets[c] += 1
+                
+      
+        total = 0
+        for h in range(n, -1, -1):
+            total += buckets[h]
+            if total >= h:
+                return h
+                
+        return 0
+
+
+        
+        
